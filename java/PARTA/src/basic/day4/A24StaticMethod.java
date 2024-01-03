6262666a838c7d135c1d9be01182f83d3b7b37d9
jooy-bit <hjooy6099@gmail.com>
2024-01-03 11:17:14 +0900

    Static method

--- java/PARTA/src/basic/day4/A24StaticMethod.java
+++ java/PARTA/src/basic/day4/A24StaticMethod.java
@@ -1,12 +1,23 @@
 package basic.day4;
 
-public class A24StacticMethod {
-    argumentTest();
-    argumentTest(i:999);
-    argumentTest(i:40,j:3);
+public class A24StaticMethod {
+    
     //메소드가 실행될 떄 실매개 변수 값이 형식매개변수로 전달, 저장됩니다.
+    /**
+     * @param args
+     */
     public static void main(String[] args){
-            //리턴이 없는 메소드 = 인자로 선언된 ()안 변수는 메소드의 지역변수
+    argumentTest();
+    argumentTest(8,4);
+    int result = resultIntTest();
+    System.out.println("resultIntTest() : "+result);
+    boolean the = resultBooleanTest();
+    System.out.println(the);
+    
+
+
+}
+        //리턴이 없는 메소드 = 인자로 선언된 ()안 변수는 메소드의 지역변수
         //인자가 없는 메소드
         public static void argumentTest(){
             System.out.println("인자값 없는 메소드");
@@ -31,13 +42,14 @@ public class A24StacticMethod {
             return false;
         }
         //리턴값이 String인 메소드
-        public static String  resultStringTest(){
+        public static String resultStringTest(){
             return "Hello";
         }
         //리턴값이 char인 메소드
-        public static char  resultCharTest(){
+        public static char resultCharTest(){
             return 'A';
         }
 
-    }//
+        
+
 }//
